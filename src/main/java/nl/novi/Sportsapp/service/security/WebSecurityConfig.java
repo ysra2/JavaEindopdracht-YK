8c1fdba223ce3e699a9d5aed196fe0b1b1b59a63
Yousra Khayati <y.khayati@novi-eduction.nl>
2021-02-02 00:50:32 +0100

    Cors controleren

--- src/main/java/nl/novi/Sportsapp/service/security/WebSecurityConfig.java
+++ src/main/java/nl/novi/Sportsapp/service/security/WebSecurityConfig.java
@@ -58,8 +58,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
-                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .exceptionHandling()
+                .authenticationEntryPoint(unauthorizedHandler)
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/user_sports/**")
